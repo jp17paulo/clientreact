1129533a6d94b7ae92dfe29fd04a5788d8e74d8a
João Paulo <jp17paulo@terra.com.br>
2024-10-14 21:24:43 -0300

    feat: implementação do roteamento para tela de login e também para a tela de alunos

---
+++ src/pages/Alunos/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
+import { FiXCircle, FiEdit, FiUserX } from "react-icons/fi";
+import logoCadastro from "../../assets/cadastro.png";
+
+export default function Alunos() {
+  return (
+    <div className="aluno-container">
+      <header>
+        <img src={logoCadastro} alt="Cadastro" />
+        <span>
+          Bem-Vindo, <strong>João Paulo</strong>!
+        </span>
+        <Link className="button" to="aluno/novo">
+          Novo Aluno
+        </Link>
+        <button type="button">
+          <FiXCircle size={35} color="#17202a" />
+        </button>
+      </header>
+      <form>
+        <input type="text" placeholder="Nome" />
+        <button type="button" className="button">
+          Filtrar aluno por nome (parcial)
+        </button>
+      </form>
+      <h1>Relação de Alunos</h1>
+      <ul>
+        <li>
+          <b>Nome:</b> Paulo
+          <br />
+          <br />
+          <b>Email:</b> paulo@email.com
+          <br />
+          <br />
+          <b>Idade:</b> 22
+          <br />
+          <br />
+          <button type="button">
+            <FiEdit size="25" color="#17202a" />
+          </button>
+          <button type="button">
+            <FiUserX size="25" color="#17202a" />
+          </button>
+        </li>
+        <li>
+          <b>Nome:</b> Paulo
+          <br />
+          <br />
+          <b>Email:</b> paulo@email.com
+          <br />
+          <br />
+          <b>Idade:</b> 22
+          <br />
+          <br />
+          <button type="button">
+            <FiEdit size="25" color="#17202a" />
+          </button>
+          <button type="button">
+            <FiUserX size="25" color="#17202a" />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
